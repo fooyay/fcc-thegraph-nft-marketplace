f9a98d2df4c6cd0f037f200efe6d527ec2a56db3
Sean Coates <sean.coates+fooyay@gmail.com>
2023-01-19 10:03:34 -0500

    integrate with Moralis cloud, which no longer exists, so, mostly an exercise

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
